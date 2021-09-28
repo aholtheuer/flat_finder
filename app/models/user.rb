e1b633947243a0e4e0206e3154965abc5958fc48
Andres Holtheuer <aholtheuer1@uc.cl>
2021-09-28 12:43:14 -0300

    User Sign up Logic

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {minimum: 3, maximum: 105},
+                   uniqueness: true,
+                   format:     {with: VALID_EMAIL_REGEX}
+
+end
