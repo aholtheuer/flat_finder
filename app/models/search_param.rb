d5517beaaad998fed4aa60e9f657dcbf3778eb1b
Andres Holtheuer <aholtheuer1@uc.cl>
2021-09-29 14:24:00 -0300

    Added one to many relation User SearchParams

--- app/models/search_param.rb
+++ app/models/search_param.rb
@@ -1,3 +1,4 @@
 class SearchParam < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maximum: 105}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :search_param
   validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {minimum: 3, maximum: 105},
