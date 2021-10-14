f4c22a56b72693644edbcae489f9fd34d8ecfffe
Andres Holtheuer <aholtheuer1@uc.cl>
2021-10-14 10:30:43 -0300

    Deleted run_scrapper.rb -- Crushing Production

--- app/scrapper/run_scrapper.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'spider.rb'
-
-url = "https://www.portalinmobiliario.com/arriendo/departamento/las-condes-metropolitana/_PriceRange_800000CLP-0CLP_FULL*BATHROOMS_2-2"
-PortalSpider.parse!(:parse, url: url)
-
